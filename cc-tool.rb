9d577f0d203a9ce11a842c879c24ab1c1fdee9cd
shariat <shariat@Treata.local>
2013-06-13 18:40:28 -0400

    upgrade cc-tool to 0.26

--- cc-tool.rb
+++ cc-tool.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class CcTool < Formula
   homepage 'http://cctool.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/cctool/cc-tool-0.24-src.tgz'
-  sha1 'c10c0d7652769ed754c549fd5f428a3519a4a78f'
+  url 'http://downloads.sourceforge.net/project/cctool/cc-tool-0.26-src.tgz'
+  sha1 'f313e55f019ea5338438633f5b5e689b699343e1'
 
   depends_on 'libusb'
   depends_on 'boost'
